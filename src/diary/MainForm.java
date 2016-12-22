f9a00867c643fbe978c17db2bcb8f34c0a7169af
Alexander Sychev <sandyqwe@yandex.ru>
2016-12-22 08:40:02 +0500

    start doing image sample tutorial

---
+++ src/diary/MainForm.java
@@ -0,0 +1,18 @@
+package diary;
+
+// from http://opencv-java-tutorials.readthedocs.io/en/latest/05-fourier-transform.html
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class MainForm extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
