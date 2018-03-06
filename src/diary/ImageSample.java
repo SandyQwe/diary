966a569919ddb2fc8437f06aaa860fe2d147b05c
SandyQwe <sandyqwe@yandex.ru>
2018-03-06 10:54:53 +0500

    1. Create SQL-script to DB creation and initial data fill

--- src/diary/ImageSample.java
+++ src/diary/ImageSample.java
@@ -1,20 +1,21 @@
 package diary;
-
+/*
 import org.opencv.core.Core;
 import org.opencv.core.Mat;
 import org.opencv.imgcodecs.Imgcodecs;
 import org.opencv.imgproc.Imgproc;
-
+*/
 
 public class ImageSample {
 
     public static void main(String[] args) {
-        System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+       /* System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
 
         Mat image = Imgcodecs.imread("resources/lena.png", 1);
         Mat grayImage = new Mat();
         Imgproc.cvtColor(image, grayImage, Imgproc.COLOR_BGR2GRAY);
         Imgcodecs.imwrite("resources/lena_gray.png", grayImage);
+        */
 
     }
 }
